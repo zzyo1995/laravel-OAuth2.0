bf46e3372aa7599905258d4e0e2ec55ee7a4a778
zzyo1995 <zzyo@hust.edu.cn>
2017-04-28 19:41:28 +0800

    upadte

---
+++ authServer/.env.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'DB_NAME'=>'oauthdb',
+    'DB_USERNAME'=>'root',
+    'DB_PASSWORD'=>'123456',
+    'HOST'=>'smtp.nfschina.com',
+    'email_passwd'=>'nfs12345',
+    'email' => 'netaccountadmin@nfschina.com',
+    'RABBITMQ_HOST' => 'localhost',
+	'RABBITMQ_PORT' => '5672',
+	'RABBITMQ_USER' => 'iscas',
+	'RABBITMQ_PASSWORD' => 'iscas321'
+);
+?>
